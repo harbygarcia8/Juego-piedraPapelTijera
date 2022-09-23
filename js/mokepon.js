46e11aff5863230a9f8d5fbe97d4c96008acb322
Harby García <harby-96@hotmail.com>
2022-09-23 16:58:16 -0500

    Se organizan las variables al escoger mascota y al escoger el ataque.

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,14 +1,31 @@
-function loadFullScreen() {
+'use strict'
+
+let ataqueJugador;
+let ataqueEnemigo;
+
+let spanResultadoJuego = document.getElementById('resultadoJuego')
+
+window.addEventListener('load', iniciarJuego);
+
+function iniciarJuego() {
 let botonMascotaJugador = document.getElementById('botonSelecionarMascota');
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador); 
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+
+let botonAtaqueFuego = document.getElementById('botonAtaqueFuego');
+botonAtaqueFuego.addEventListener('click', ataqueFuego);
+
+let botonAtaqueAgua = document.getElementById('botonAtaqueAgua');
+botonAtaqueAgua.addEventListener('click', ataqueAgua);
+
+let botonAtaqueTierra = document.getElementById('botonAtaqueTierra');
+botonAtaqueTierra.addEventListener('click', ataqueTierra);
 }
-window.addEventListener('load', loadFullScreen);
+
 function aleatorio(min,max) {
     return Math.floor(Math.random()*(max - min + 1)+min);
 }
 
-
-function seleccionarMascotaJugador () {
+function seleccionarMascotaJugador() {
     let hipodoge = document.getElementById('hipodoge');
     let capipepo = document.getElementById('capipepo')
     let ratigueya = document.getElementById('ratigueya')
@@ -34,31 +51,59 @@ function seleccionarMascotaJugador () {
         alert("No seleccionaste ninguna mascota")
     }
 
-    mascotaEnemigo();
+    seleccionarMascotaEnemigo();
 }
 
-function mascotaEnemigo () {
-    let ataqueAleatorio = aleatorio(1,6);
+function seleccionarMascotaEnemigo () {
+    let mascotaAleatoria = aleatorio(1,6);
     let spanMascotaEnemigo = document.getElementById('nombreMascotaEnemigo');
-
-    switch (ataqueAleatorio) {
+    switch (mascotaAleatoria) {
         case 1: spanMascotaEnemigo.innerHTML = "Hipodoge"
         break;
-
         case 2: spanMascotaEnemigo.innerHTML = "Capipepo"
         break;
-
         case 3: spanMascotaEnemigo.innerHTML = "Ratigueya"
         break;
-
         case 4: spanMascotaEnemigo.innerHTML = "Langostelvis"
         break;
-
         case 5: spanMascotaEnemigo.innerHTML = "Tucapalma"
         break;
-
         case 6: spanMascotaEnemigo.innerHTML = "Pydos "
         break;
     }
 
+}
+
+function ataqueFuego() {
+    let spanResultadoAtaqueJugador = document.getElementById('resultadoAtaqueJugador');
+    ataqueJugador = 'TU ATAQUE ES: FUEGO \n';
+    spanResultadoAtaqueJugador.innerHTML = 'FUEGO';
+    ataqueAleatorioEnemigo();
+}
+
+function ataqueAgua() {
+    let spanResultadoAtaqueJugador = document.getElementById('resultadoAtaqueJugador');
+    ataqueJugador = 'TU ATAQUE ES: AGUA \n';
+    spanResultadoAtaqueJugador.innerHTML = 'AGUA';
+    ataqueAleatorioEnemigo();
+}
+
+function ataqueTierra() {
+    let spanResultadoAtaqueJugador = document.getElementById('resultadoAtaqueJugador');
+    ataqueJugador = 'TU ATAQUE ES: TIERRA \n';
+    spanResultadoAtaqueJugador.innerHTML = 'TIERRA';
+    ataqueAleatorioEnemigo();
+}
+
+function ataqueAleatorioEnemigo() {
+    ataqueEnemigo = aleatorio (1,3);
+    let spanResultadoAtaqueEnemigo = document.getElementById('resultadoAtaqueEnemigo');
+    switch (ataqueEnemigo) {
+        case 1: ataqueEnemigo = 'El ataque enemigo es: FUEGO'; spanResultadoAtaqueEnemigo.innerHTML = "FUEGO";
+        break;
+        case 2: ataqueEnemigo = 'El ataque enemigo es: AGUA'; spanResultadoAtaqueEnemigo.innerHTML = "AGUA";
+        break;
+        case 3: ataqueEnemigo = 'El ataque enemigo es: TIERRA'; spanResultadoAtaqueEnemigo.innerHTML = "TIERRA";
+        break;
+    }
 }
